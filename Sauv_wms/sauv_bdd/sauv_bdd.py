fba8d9916e331e25e0e4e4768d1fd3ca3dd83476
Matteo <152607865+Mattewo-cmd@users.noreply.github.com>
2026-03-13 11:22:24 +0100

    Update sauv_bdd.py

--- Sauv_wms/sauv_bdd/sauv_bdd.py
+++ Sauv_wms/sauv_bdd/sauv_bdd.py
@@ -8,8 +8,29 @@ if chemin_connexion not in sys.path:
     sys.path.insert(0, chemin_connexion)
 
 # Importer ta fonction de connexion
-from connex_bdd import connect_to_database
+# from connex_bdd import connect_to_database
+def connect_to_database():
+    print("\n--- Connexion MySQL ---")
+    host = input("  Hôte / IP  : ").strip() or "WMS-APP"
+    user = input("  User [mspr] : ").strip() or "mspr"
+    password = getpass.getpass("  Password : ") # mspr
+    database = input("  Base [WMS] : ").strip() or "WMS"
 
+    try:
+        # Connexion classique sans options de curseur spéciales
+        conn = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database,
+            port=3306,
+            connect_timeout=10
+        )
+        print("Connexion OK !\n")
+        return conn
+    except Exception as e:
+        print(f"[ERREUR] : {e}")
+        return None
 # ============================================
 # FONCTION 1 : Créer le dossier backups
 # ============================================
